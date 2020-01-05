7edbd2e780b42142a57372fec40904f31dc639ee
tajud99n <giwa208@gmail.com>
2020-01-05 08:57:40 +0100

    mocking unit test for external API calls

--- src/api/providers/locations/locations_test.go
+++ src/api/providers/locations/locations_test.go
@@ -2,12 +2,26 @@ package locations
 
 import (
 	"net/http"
+	"os"
 	"testing"
 
+	"github.com/mercadolibre/golang-restclient/rest"
 	"github.com/stretchr/testify/assert"
 )
 
+func TestMain(m *testing.M)  {
+	rest.StartMockupServer()
+	os.Exit(m.Run())
+}
+
 func TestGetCountryRestClientError(t *testing.T) {
+	rest.FlushMockups()
+	rest.AddMockups(&rest.Mock{
+		URL:          "https://api.mercadolibre.com/countries/AR",
+		HTTPMethod:   http.MethodGet,
+		RespHTTPCode: 0,
+	})
+
 	country, err := GetCountry("AR")
 
 	assert.Nil(t, country)
@@ -17,6 +31,14 @@ func TestGetCountryRestClientError(t *testing.T) {
 }
 
 func TestGetCountryNotFound(t *testing.T) {
+	rest.FlushMockups()
+	rest.AddMockups(&rest.Mock{
+		URL:          "https://api.mercadolibre.com/countries/ARS",
+		HTTPMethod:   http.MethodGet,
+		RespHTTPCode: http.StatusNotFound,
+		RespBody:     `{"message": "Country not found", "error": "not_found", "status": 404}`,
+	})
+
 	country, err := GetCountry("ARS")
 
 	assert.Nil(t, country)
@@ -26,15 +48,31 @@ func TestGetCountryNotFound(t *testing.T) {
 }
 
 func TestGetCountryInvalidErrorInterface(t *testing.T) {
+	rest.FlushMockups()
+	rest.AddMockups(&rest.Mock{
+		URL:          "https://api.mercadolibre.com/countries/AR",
+		HTTPMethod:   http.MethodGet,
+		RespHTTPCode: http.StatusNotFound,
+		RespBody:     `{"message": "Country not found", "error": "not_found", "status": "404"}`,
+	})
+
 	country, err := GetCountry("AR")
 
 	assert.Nil(t, country)
 	assert.NotNil(t, err)
 	assert.EqualValues(t, http.StatusInternalServerError, err.Status)
-	assert.EqualValues(t, "invalid error interface when getting country AR", err.Message)
+	assert.EqualValues(t, "invalid error response when getting country AR", err.Message)
 }
 
 func TestGetCountryInvalidJSONResponse(t *testing.T) {
+	rest.FlushMockups()
+	rest.AddMockups(&rest.Mock{
+		URL:          "https://api.mercadolibre.com/countries/AR",
+		HTTPMethod:   http.MethodGet,
+		RespHTTPCode: http.StatusOK,
+		RespBody:     `{"id": 123, "name": "Argentina", "time_zone": "GMT-03:00"}`,
+	})
+
 	country, err := GetCountry("AR")
 
 	assert.Nil(t, country)
@@ -44,6 +82,14 @@ func TestGetCountryInvalidJSONResponse(t *testing.T) {
 }
 
 func TestGetCountryNoError(t *testing.T) {
+	rest.FlushMockups()
+	rest.AddMockups(&rest.Mock{
+		URL:          "https://api.mercadolibre.com/countries/AR",
+		HTTPMethod:   http.MethodGet,
+		RespHTTPCode: http.StatusOK,
+		RespBody:     `{"id": "AR", "name": "Argentina", "time_zone": "GMT-03:00"}`,
+	})
+
 	country, err := GetCountry("AR")
 
 	assert.Nil(t, err)
