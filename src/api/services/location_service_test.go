a654e0ea3104d381c2aed894f25420f149d70ba1
tajud99n <giwa208@gmail.com>
2020-01-06 09:18:16 +0100

    integration test for location service

--- src/api/services/location_service_test.go
+++ src/api/services/location_service_test.go
@@ -1,15 +1,27 @@
 package services
 
 import (
-	"fmt"
 	"net/http"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/mercadolibre/golang-restclient/rest"
 )
 
-func TestGetCountry(t *testing.T) {
+func TestMain(m *testing.M)  {
+	rest.StartMockupServer()
+	os.Exit(m.Run())
+}
+
+func TestGetCountryRestClientError(t *testing.T) {
 	ls := locationService{}
+	rest.FlushMockups()
+	rest.AddMockups(&rest.Mock{
+		URL:          "https://api.mercadolibre.com/countries/AR",
+		HTTPMethod:   http.MethodGet,
+		RespHTTPCode: 0,
+	})
 	country, err := ls.GetCountry("AR")
 
 	if country != nil {
@@ -18,6 +30,23 @@ func TestGetCountry(t *testing.T) {
 
 	assert.EqualValues(t, http.StatusInternalServerError, err.Status)
 	assert.EqualValues(t, "invalid restclient response when trying to get country AR", err.Message)
+}
 
-	fmt.Println(err)
+func TestGetCountryNoError(t *testing.T) {
+	ls := locationService{}
+	rest.FlushMockups()
+	rest.AddMockups(&rest.Mock{
+		URL:          "https://api.mercadolibre.com/countries/AR",
+		HTTPMethod:   http.MethodGet,
+		RespHTTPCode: http.StatusOK,
+		RespBody:     `{"id": "AR", "name": "Argentina", "time_zone": "GMT-03:00"}`,
+
+	})
+	country, err := ls.GetCountry("AR")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	assert.EqualValues(t, "AR", country.Id)
 }
